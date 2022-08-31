dc6c405443e0be6d402a83da88dfb8203836bddf
Nitis <reberrus@gmail.com>
2022-08-31 21:40:11 +0700

    Test3

--- fillStartWord.js
+++ fillStartWord.js
@@ -3,16 +3,18 @@ const { template } = require('@babel/core')
 function fillStartWord(startWord, word) {
     if (word == null || word == undefined) {
         console.log(undefined)
+        return undefined
     }
 
     else if (startWord.length == word.length){
         if (startWord == word){
             console.log(word)
+            return String(word) 
         }
     }
 
     else {
-        let str = []
+        let str = ""
         for (let index = 0; index < startWord.length; index++) {
             if (startWord[index] != word[index]){
             str += startWord[index]
@@ -22,7 +24,7 @@ function fillStartWord(startWord, word) {
             str += word[index]
         }    
         console.log(str)
+        return String(str)  
     }
 }
-
 module.exports = fillStartWord
